4ab3f10d3867188279c9840a46b826b1afc107fe
Boun13a <mo.halbouni@gmail.com>
2022-02-06 22:29:39 -0500

    Fixed the Up button navigation

--- Frontend/HandyApp/app/src/main/java/com/example/handyapp/MainActivity.java
+++ Frontend/HandyApp/app/src/main/java/com/example/handyapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.handyapp;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 import android.view.MenuItem;
@@ -11,19 +12,60 @@ import android.view.View;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 
+import java.util.Stack;
+
 public class MainActivity extends AppCompatActivity {
 
+    Stack <Fragment> stack;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        stack = new Stack<Fragment>();
         setContentView(R.layout.activity_main);
 
 
         BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
         bottomNav.setOnItemSelectedListener(navListener);
         getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new HomeFragment()).commit();
+
+
+
     }
 
+    public void pushFragment(Fragment fragment, boolean add) {
+        FragmentTransaction transation = getSupportFragmentManager()
+                .beginTransaction();
+        if (add)
+            stack.push(fragment);
+        transation.replace(R.id.fragment_container, fragment);
+        transation.commit();
+    }
+
+    public void popFragment() {
+        if (!stack.isEmpty()) {
+            Fragment fragment = stack.elementAt(stack.size() - 2);
+            stack.pop();
+            pushFragment(fragment, false);
+        } else
+            super.onBackPressed();
+        //drawerToggler.setDrawerIndicatorEnabled(stack.size() == 1);
+    }
+
+    public void clearBackStack() {
+        stack.clear();
+    }
+
+
+    public void makingFragment(Fragment fragment)
+    {
+
+        FragmentTransaction t = getSupportFragmentManager().beginTransaction();
+        t.replace(R.id.fragment_container,fragment);
+        t.setReorderingAllowed(true);
+        t.addToBackStack("Fragmito");
+        t.commit();
+
+    }
 
 
     private NavigationBarView.OnItemSelectedListener navListener = new NavigationBarView.OnItemSelectedListener() {
@@ -35,15 +77,19 @@ public class MainActivity extends AppCompatActivity {
             {
                 case R.id.nav_home:
                     selectedFragment = new HomeFragment();
+
                     break;
                 case R.id.nav_inbox:
                     selectedFragment = new InboxFragment();
+
                     break;
                 case R.id.nav_search:
                     selectedFragment = new SearchFragment();
+
                     break;
                 case R.id.nav_profile:
                     selectedFragment = new ProfileFragment();
+
                     break;
             }
 
@@ -57,42 +103,48 @@ public class MainActivity extends AppCompatActivity {
 
     public void whichTextClicked(View view)
     {
-        Fragment childFragment = null;
+        //Fragment childFragment = null;
         switch (view.getId())
         {
             case R.id.carRental:
                 //Toast.makeText(getApplicationContext(), "Car Rental is clicked", Toast.LENGTH_SHORT).show();
-                childFragment = new CarRental();
+                CarRental c = new CarRental();
+                makingFragment(c);
                 break;
 
 
             case R.id.housePainting:
                 //Toast.makeText(getApplicationContext(), "House painting is clicked", Toast.LENGTH_SHORT).show();
-                childFragment = new housePainting();
+                housePainting h = new housePainting();
+                makingFragment(h);
                 break;
 
 
             case R.id.foodService:
-                childFragment = new FoodService();
+                FoodService f = new FoodService();
+                makingFragment(f);
                 break;
 
 
             case R.id.cleaningService:
-                childFragment = new CleaningService();
+                CleaningService cs = new CleaningService();
+                makingFragment(cs);
                 break;
 
 
             case R.id.photography:
-                childFragment = new Photography();
+                Photography ph = new Photography();
+                makingFragment(ph);
                 break;
 
 
             case R.id.plumbing:
-                childFragment = new Plumbing();
+                Plumbing pl = new Plumbing();
+                makingFragment(pl);
                 break;
         }
 
-        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,childFragment).commit();
+        //getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,childFragment).commit();
     }
 
 }

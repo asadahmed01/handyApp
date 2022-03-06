d01de5950b39302079fc0c3f9b868f0b553f8529
Mohammed Abusultan <Mabusultan5425@conestogac.on.ca>
2022-03-06 17:42:03 -0500

    edited SellerAddFragment java file and fixed onViewCreated function

--- Frontend/HandyApp_V2/app/src/main/java/Activities/fragments/SellerAddFragment.java
+++ Frontend/HandyApp_V2/app/src/main/java/Activities/fragments/SellerAddFragment.java
@@ -2,11 +2,14 @@ package Activities.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 
 import com.example.handyapp_v2.R;
 
@@ -16,13 +19,13 @@ import com.example.handyapp_v2.R;
  * create an instance of this fragment.
  */
 public class SellerAddFragment extends Fragment {
+    private RelativeLayout plumbing, cleaning, photography, carRepair, painting, house, web, video;
 
-    // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
+
     private String mParam1;
     private String mParam2;
 
@@ -30,15 +33,8 @@ public class SellerAddFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment SellerAddFragment.
-     */
-    // TODO: Rename and change types and number of parameters
+
+
     public static SellerAddFragment newInstance(String param1, String param2) {
         SellerAddFragment fragment = new SellerAddFragment();
         Bundle args = new Bundle();
@@ -56,6 +52,26 @@ public class SellerAddFragment extends Fragment {
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        plumbing = view.findViewById(R.id.plumbing);
+        cleaning = view.findViewById(R.id.cleaning);
+        photography = view.findViewById(R.id.photo);
+        carRepair = view.findViewById(R.id.car);
+        painting = view.findViewById(R.id.paint);
+        house = view.findViewById(R.id.house);
+        web = view.findViewById(R.id.web);
+        video = view.findViewById(R.id.video);
+
+
+        clickListner();
+
+
+    }
+
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,

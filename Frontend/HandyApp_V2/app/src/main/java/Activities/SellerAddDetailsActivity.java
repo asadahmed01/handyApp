99216db3ec2b98fbfa65784e761e3426693e1675
Mohammed Abusultan <Mabusultan5425@conestogac.on.ca>
2022-03-06 17:58:21 -0500

    getting all elemts by ID to create upload function for SellerAddDetailsActivity

--- Frontend/HandyApp_V2/app/src/main/java/Activities/SellerAddDetailsActivity.java
+++ Frontend/HandyApp_V2/app/src/main/java/Activities/SellerAddDetailsActivity.java
@@ -3,14 +3,43 @@ package Activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.handyapp_v2.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class SellerAddDetailsActivity extends AppCompatActivity {
+
+    EditText price,skills,description;
+    String Price, uid;
+    String category;
+    Button upload;
+    FirebaseAuth firebaseAuth;
+    FirebaseFirestore firebaseFirestore;
+    String currentDate,currentTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_seller_add_details);
+
+        //find all elements by ID
+        price = findViewById(R.id.price);
+        skills = findViewById(R.id.skills);
+        description = findViewById(R.id.description);
+        Price = price.getText().toString();
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseFirestore = FirebaseFirestore.getInstance();
+        uid = firebaseAuth.getCurrentUser().getUid();
+
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault());
+        String currentDateandTime = sdf.format(new Date());
+        
     }
 }

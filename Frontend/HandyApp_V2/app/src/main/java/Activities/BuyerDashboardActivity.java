ee1e2907fd5d0fcfd90beb5e6bd69935731ee659
Mohammed Abusultan <Mabusultan5425@conestogac.on.ca>
2022-03-06 03:43:50 -0500

    fixed onCreate function in BuyerDashboard Activity

--- Frontend/HandyApp_V2/app/src/main/java/Activities/BuyerDashboardActivity.java
+++ Frontend/HandyApp_V2/app/src/main/java/Activities/BuyerDashboardActivity.java
@@ -1,16 +1,72 @@
 package Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
+import android.widget.FrameLayout;
 
 import com.example.handyapp_v2.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.tabs.TabLayout;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import Activities.fragments.MessagesFragment;
 
 public class BuyerDashboardActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
+    private TabLayout tabLayout;
+    private List<Fragment> fragmentList;
+    private FrameLayout frameLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_buyer_dashboard);
+
+        frameLayout = findViewById(R.id.framlayout);
+        tabLayout = findViewById(R.id.tablayout);
+
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            tabLayout.getTabAt(2).getIcon().setTintList(ColorStateList.valueOf(Color.parseColor("#BDBDBD")));
+        }
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                //setFragment(tab.getPosition());
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    tab.getIcon().setTintList(ColorStateList.valueOf(getResources().getColor(R.color.red_d86041)));
+                }
+
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    tab.getIcon().setTintList(ColorStateList.valueOf(Color.parseColor("#BDBDBD")));
+                }
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            tabLayout.getTabAt(0).getIcon().setTintList(ColorStateList.valueOf(getResources().getColor(R.color.red_light_ffe0de)));
+        }
+        //setFragment(0);
+
+    }
+
     }
 }

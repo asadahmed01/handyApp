8db8a4543550e1e87f2a26ba093416be7a2581a1
Mohammed Abusultan <Mabusultan5425@conestogac.on.ca>
2022-03-07 22:58:33 -0500

    added retrive function to serchfragment

--- Frontend/HandyApp_V2/app/src/main/java/Activities/fragments/SearchFragment.java
+++ Frontend/HandyApp_V2/app/src/main/java/Activities/fragments/SearchFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.text.Editable;
@@ -15,7 +16,11 @@ import android.view.ViewGroup;
 import android.widget.EditText;
 
 import com.example.handyapp_v2.R;
+import com.google.firebase.firestore.DocumentChange;
+import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
 
@@ -52,18 +57,21 @@ public class SearchFragment extends Fragment {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
+                RetreaveFirestore();
 
             }
 
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
 
+                RetreaveFirestore();
 
             }
 
             @Override
             public void afterTextChanged(Editable s) {
 
+                RetreaveFirestore();
 
             }
 
@@ -71,4 +79,44 @@ public class SearchFragment extends Fragment {
 
 
     }
+
+    private void RetreaveFirestore() {
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        list = new ArrayList<ListModel>();
+        myAdapter = new ListAdapter(getContext(), list);
+
+        recyclerView.setAdapter(myAdapter);
+
+        EventChildListner();
+    }
+
+    private void EventChildListner() {
+        String Search = "";
+        Search = search.getText().toString();
+        firestore.collection("data")
+                .whereEqualTo("username", Search)
+                .addSnapshotListener(new EventListener<QuerySnapshot>() {
+                    @Override
+                    public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
+                        if (error != null) {
+
+                        }
+                        for (DocumentChange dc : value.getDocumentChanges()) {
+                            if (dc.getType() == DocumentChange.Type.ADDED) {
+                                list.add(dc.getDocument().toObject(ListModel.class));
+
+                            }
+                            myAdapter.notifyDataSetChanged();
+                        }
+                    }
+                });
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        RetreaveFirestore();
+    }
+
 }

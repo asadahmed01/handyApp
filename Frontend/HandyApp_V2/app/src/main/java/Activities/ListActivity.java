0c83cc3281176d5d30f831ba0a20c5a079fdddde
asadahmed01 <asadatnish@gmail.com>
2022-03-07 20:47:00 -0500

    add the model class for the sellers screen

--- Frontend/HandyApp_V2/app/src/main/java/Activities/ListActivity.java
+++ Frontend/HandyApp_V2/app/src/main/java/Activities/ListActivity.java
@@ -4,7 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.handyapp_v2.R;
+
 public class ListActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {

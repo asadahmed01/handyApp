6dcb30625f9fd83f566778d5c427233aa27be9df
Mohammed Abusultan <Mabusultan5425@conestogac.on.ca>
2022-03-07 22:14:40 -0500

    added onViewCreated to searchFragment

--- Frontend/HandyApp_V2/app/src/main/java/Activities/fragments/SearchFragment.java
+++ Frontend/HandyApp_V2/app/src/main/java/Activities/fragments/SearchFragment.java
@@ -2,9 +2,13 @@ package Activities.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -32,5 +36,39 @@ public class SearchFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_search, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
+        recyclerView = view.findViewById(R.id.recycleView);
+        search = view.findViewById(R.id.search);
+        firestore = FirebaseFirestore.getInstance();
+
+
+        search.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+
+            }
+
+        });
+
+
     }
 }

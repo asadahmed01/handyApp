ced1f5885496e77897f0b30b30bf70733757b041
Mohammed Abusultan <Mabusultan5425@conestogac.on.ca>
2022-03-08 23:13:00 -0500

    edited layout for editProfile activity

---
+++ Frontend/HandyApp_V2/app/src/main/java/Activities/EditProfileActivity.java
@@ -0,0 +1,16 @@
+package Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.handyapp_v2.R;
+
+public class EditProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_profile);
+    }
+}

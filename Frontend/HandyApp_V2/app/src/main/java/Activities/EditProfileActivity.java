fe9bd1c0449eda7a896a9eb8b64ab762139bf56a
Mohammed Abusultan <Mabusultan5425@conestogac.on.ca>
2022-03-08 23:29:14 -0500

    added update password function

--- Frontend/HandyApp_V2/app/src/main/java/Activities/EditProfileActivity.java
+++ Frontend/HandyApp_V2/app/src/main/java/Activities/EditProfileActivity.java
@@ -1,16 +1,64 @@
 package Activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.handyapp_v2.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.EmailAuthProvider;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class EditProfileActivity extends AppCompatActivity {
+
+    EditText address, password;
+    Button update;
+    String Password;
+    String Address;
+    String oldpass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
+        address = findViewById(R.id.address);
+        password = findViewById(R.id.password);
+        update = findViewById(R.id.update);
+
+    }
+
+    private void UpdatePassword(String password, String address, String oldpass) {
+        FirebaseUser user;
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        final String email = user.getEmail();
+        AuthCredential credential = EmailAuthProvider.getCredential(email, oldpass);
+
+        user.reauthenticate(credential).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if (task.isSuccessful()) {
+                    user.updatePassword(password).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if (!task.isSuccessful()) {
+                                Toast.makeText(EditProfileActivity.this, "Something went wrong. Please try again later", Toast.LENGTH_SHORT).show();
+                            } else {
+                                //get address here
+                                Toast.makeText(EditProfileActivity.this, "Password Successfully Modified", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                } else {
+                    Toast.makeText(EditProfileActivity.this, "Authentication Failed", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }

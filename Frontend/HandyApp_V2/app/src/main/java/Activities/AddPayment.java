747f4150d8fea8db8f681bde65bd96c772b64047
Boun13a <mo.halbouni@gmail.com>
2022-03-13 19:05:31 -0400

    set onclickListener event for the save button

--- Frontend/HandyApp_V2/app/src/main/java/Activities/AddPayment.java
+++ Frontend/HandyApp_V2/app/src/main/java/Activities/AddPayment.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import com.example.handyapp_v2.R;
@@ -26,5 +27,11 @@ public class AddPayment extends AppCompatActivity {
 
         cardMultilineWidget = findViewById(R.id.card_input_widget);
         save = findViewById(R.id.save_payment);
+
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            }
+        });
     }
 }

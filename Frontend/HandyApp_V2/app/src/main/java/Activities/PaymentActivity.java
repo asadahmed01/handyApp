c3a1e7f2620cf8220979196bd472e40960e4dbb5
Mohammed Abusultan <Mabusultan5425@conestogac.on.ca>
2022-03-12 19:52:13 -0500

    payment activity added

---
+++ Frontend/HandyApp_V2/app/src/main/java/Activities/PaymentActivity.java
@@ -0,0 +1,17 @@
+package Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.handyapp_v2.R;
+
+public class PaymentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_payment);
+        
+    }
+}

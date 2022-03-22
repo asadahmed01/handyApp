61346b5b4a8a17bb1c83af87e13b145370b6e328
Mohammed Abusultan <Mabusultan5425@conestogac.on.ca>
2022-03-21 23:13:28 -0400

    edited layout for splash screen

--- Frontend/HandyApp_V2/app/src/main/java/Activities/AddPayment.java
+++ Frontend/HandyApp_V2/app/src/main/java/Activities/AddPayment.java
@@ -1,6 +1,9 @@
 package Activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -9,7 +12,10 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.handyapp_v2.R;
+import com.stripe.android.Stripe;
+import com.stripe.android.TokenCallback;
 import com.stripe.android.model.Card;
+import com.stripe.android.model.Token;
 import com.stripe.android.view.CardMultilineWidget;
 
 public class AddPayment extends AppCompatActivity {
@@ -23,9 +29,9 @@ public class AddPayment extends AppCompatActivity {
         setContentView(R.layout.activity_add_payment);
         setContentView(R.layout.activity_add_payment);
         ActionBar actionBar = getSupportActionBar();
-        actionBar.setTitle("Payment");
-        actionBar.setDisplayHomeAsUpEnabled(true);
-        actionBar.setDisplayShowHomeEnabled(true);
+       // actionBar.setTitle("Payment");
+       // actionBar.setDisplayHomeAsUpEnabled(true);
+       // actionBar.setDisplayShowHomeEnabled(true);
 
         cardMultilineWidget = findViewById(R.id.card_input_widget);
         save = findViewById(R.id.save_payment);
@@ -51,6 +57,48 @@ public class AddPayment extends AppCompatActivity {
             }
         }
     }
+
+    private void CreateToken( Card card) {
+        Stripe stripe = new Stripe(getApplicationContext(), getString(R.string.publishablekey));
+        stripe.createToken(
+                card,
+                new TokenCallback() {
+                    public void onSuccess(Token token) {
+
+                        // Send token to your server
+                        Log.e("Stripe Token", token.getId());
+                        Intent intent = new Intent();
+                        intent.putExtra("card",token.getCard().getLast4());
+                        intent.putExtra("stripe_token",token.getId());
+                        intent.putExtra("cardtype",token.getCard().getBrand());
+                        setResult(0077,intent);
+                        finish();
+                    }
+                    public void onError(Exception error) {
+
+                        // Show localized error message
+                        Toast.makeText(getApplicationContext(),
+                                error.getLocalizedMessage(),
+                                Toast.LENGTH_LONG
+                        ).show();
+                    }
+                }
+        );
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                break;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+        return true;
+    }
+
 
 
 }
